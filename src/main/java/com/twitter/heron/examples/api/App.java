b8bb3038cded474ead7992b3fcd96c2d45134e77
sixpluszero <russellspurs@gmail.com>
2018-05-03 11:01:38 -0500

    initial

---
+++ src/main/java/com/twitter/heron/examples/api/App.java
@@ -0,0 +1,13 @@
+package com.twitter.heron.examples.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
